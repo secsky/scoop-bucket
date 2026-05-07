76f18229640774556093a1da136dca1553ac8abc
secsky <hutongwenhua@gmail.com>
2026-05-07 22:59:38 +0800

    更新持久化目录

--- scripts/Update-PixPin.ps1
+++ scripts/Update-PixPin.ps1
@@ -42,7 +42,7 @@ $manifest = [ordered]@{
     url = $url
     hash = $hash
     extract_dir = "PixPin"
-    persist = @("Config", "Data", "History", "LocalStorage.data", "LoginData", "OcrModel")
+    persist = @("Config", "Data", "History")
     shortcuts = @(
         , @("PixPin.exe", "PixPin")
     )
